750b07770727bc56d10797e9be66e46680d554b5
heuristy <phk952@naver.com>
2017-02-18 01:23:17 +0900

    matrix의 크기와 곱을 분석

--- mlp.m
+++ mlp.m
@@ -12,11 +12,9 @@ end
 V = cell(L);
 V{1} = X;
 
-eta = 2/size(X,2);
- dGp = 500;
- dGm = 500;
+dGp = 1000/size(X,2);
+dGm = 1000/size(X,2);
 
-    
 maxiter = 50;
 mse = zeros(1,maxiter);
 
@@ -33,20 +31,22 @@ for iter = 1:maxiter
     E = V{L}-Y;
     mse(iter) = mean(dot(E(:),E(:)));
 
-    GxD = V{L}-Y;
+    GxD = V{L}-Y; % #(L) x m
     for l = L-1:-1:1
         
-        df = V{l+1}.*(1-V{l+1});
-        D = df.*(GxD);
+        df = V{l+1}.*(1-V{l+1}); % #(l+1) x m
+        D = df.*(GxD); % #(l+1) x m
         
-        VxD = V{l}*D';
-        dG = VxD;
+        VxD = V{l}*D'; % {#(l) x m} * {m x #(l+1)} = #(l) x #(l+1)
+        dG = VxD; % #(l) x #(l+1)
+        % 이 경우는 m개의 set들의 dG를 모두 합한 다음에 이진화를 한 것이므로 사실상 근사를 거의 안했다고 보아야 한다.
+        % 이진화를 V{l}*D'과정에서 진행하는 경우부터가 문제이다.
         dG(VxD >0) = -dGm;
         dG(VxD==0) = 0;
         dG(VxD <0) = dGp;
-        G{l} = G{l} + eta*dG;
+        G{l} = G{l} + dG;
         
-        GxD = G{l}*D;
+        GxD = G{l}*D; % #(l) x m
     end
     
     fprintf('mse : %d\n', mse(iter))
